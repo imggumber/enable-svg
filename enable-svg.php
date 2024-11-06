bde19e703c5b6e826f0164ad6239a7bbaf082d6b
Gurpreet Gumber <gumber.gurpreet@rwit.io>
2024-11-06 16:36:28 +0530

    Added plugin header file

---
+++ enable-svg.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @package enablesvg
+ */
+/*
+Plugin Name: Enable SVG with Garry
+Description: Used to enable SVG support.
+Version: 1.0.0
+Requires at least: 6.6.2
+Requires PHP: 8.1
+Author: Gurpreet Gumber
+Author URI: https://github.com/imggumber
+License: GPLv2 or later
+Text Domain: enablesvggarry
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+*/
+
+define( 'ENABLE_SVG_VERSION', '1.0.0' );
+define( 'ENABLE_SVG_MINIMUM_WP_VERSION', '6.6.2' );
+define( 'ENABLE_SVG_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+?>
